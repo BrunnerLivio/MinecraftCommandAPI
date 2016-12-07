c51bbe9cff09f7bf37ac50253786ca4613c639f2
Livio Brunner <livio.brunner@hotmail.com>
2016-12-07 21:28:27 +0100

    add entity tags

--- src/core/Entity/EntityTag.ts
+++ src/core/Entity/EntityTag.ts
@@ -3,12 +3,28 @@ interface IEntityTag {
      * The least significant bits of this entity's Universally Unique IDentifier.
      */
     UUIDLeast: String;
+    /**
+     * The most significant bits of this entity's Universally Unique IDentifier. This is joined with UUIDLeast to form this entity's unique ID
+     */
+    UUIDMost: String;
+    /**
+     * Makes mobs invulnerable
+     */
+    Invulnerable: Boolean;
 }
 
 class EntityTag implements IEntityTag {
     private uuidLeast: String;
+    private uuidMost: String;
+    private invulnerable;
     public set UUIDLeast(value: String) {
         this.uuidLeast = value;
+    }
+    public set UUIDMost(value: String) {
+        this.uuidMost = value;
+    }
+    public set Invulnerable(value: Boolean) {
+        this.invulnerable = value;
     }
 }
 
